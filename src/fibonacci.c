215f5d7067c367f4cdea0605e0ce1d0df8408da7
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-02-09 09:50:16 -0500

    final version

--- src/fibonacci.c
+++ src/fibonacci.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <wait.h>
 #include "fibonacci.h"
 
 #define get_time time(NULL)
@@ -20,7 +21,6 @@ int fib_count =  2;
 */
 int main(){
 	time_t start_s = get_time;
-
 	pid_t child1;
 	pid_t child2 = -1;
 	child1 = fork();
@@ -69,19 +69,18 @@ void printPID(){
 * safely prints the current working directory
 */
 void printCWD(){
-    char* dir =NULL;
-    dir = getcwd(dir, sizeof(dir));
+    char dir[1024];
+    getcwd(dir, sizeof(dir));
     if(dir == NULL) perror("ERROR WHILE GETTING CURRENT WORKING DIRECTORY");
     printf("CWD is %s\n",dir);
 }
 
 /**
-* safely prints the username of the user executing this thread
+* prints the username of the user executing this thread
+* This is deprecated, and works most of the time.  Seriously, screw that.
 */
 void printLogin(){
-    char * username = getlogin();
-    if(username == NULL) perror("ERROR WHILE GETTING USERNAME");
-    printf("Login: %s\n",username);
+    printf("Login: %s\n",getlogin());
     fflush(stdout);
 }
 
@@ -104,7 +103,8 @@ void printExitCodes(pid_t child1, pid_t child2){
 */
 void fib(int childNumber, time_t start_s){
     pid_t pid  = getpid();
-    for(int iterations = 0; iterations < 8; iterations++){
+    int iterations = 0;
+    for(iterations = 0; iterations < 8; iterations++){
         if(childNumber == 2)sleep(1);
         fib_count++;
         fib_sum = f1 + f2;
@@ -117,6 +117,7 @@ void fib(int childNumber, time_t start_s){
         if(childNumber == 2) sleep(1);
         if(childNumber == 1) sleep(2);
     }
+	if(childNumber == 2) sleep(1);
     printf("\nChild%d has PID %d", childNumber, getpid());
     printProcessStats(start_s);
     fflush(stdout);
@@ -132,6 +133,8 @@ void fib(int childNumber, time_t start_s){
 *       wall clock execution time
 */
 void printProcessStats(time_t startTime) {
+        int i = 0;
+        for(i = 0; i < 999999999; i++);
     int realuserid = geteuid(); //cuserid wasn't resolving...
     if(realuserid<0) perror("ERROR WHILE GETTING REAL USER ID");
 
@@ -149,12 +152,13 @@ void printProcessStats(time_t startTime) {
 
     printf("\nProcess with ID %d has:\nReal UID: %d\nEffective UID: %d\nReal GID: %d\nEffective GID: %d\n",
             pid, realuserid, effuserid, realgroupid, effgroupid);
-
     struct rusage rusage1;
-    if(getrusage(RUSAGE_SELF, &rusage1 ) == -1) perror("ERROR WHILE GETTING RESOURCE USAGE");
+    if(getrusage(RUSAGE_SELF, &rusage1 ) < 0) perror("ERROR WHILE GETTING RESOURCE USAGE");
     printf("Usertime: sec=%ld, usec=%ld\n", (long)(rusage1).ru_utime.tv_sec, (long)(rusage1).ru_utime.tv_usec);
     printf("Systime: sec=%ld, usec=%ld\n", (long)(rusage1).ru_stime.tv_sec, (long)(rusage1).ru_stime.tv_usec);
-    printf("Process took %.2f seconds to execute\n", difftime(get_time,startTime));
-
+    printf("Process took %.9f seconds to execute\n", difftime(get_time,startTime));
+    time_t currentTime;
+    time(&currentTime);
+    printf("The current time is: %s\n",ctime(&currentTime));
     fflush(stdout);
 }
