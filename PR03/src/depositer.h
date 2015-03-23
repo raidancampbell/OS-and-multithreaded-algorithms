7d9fe150dd8eab9d12f716356f170e946890dd22
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-03-23 15:00:49 -0400

    added sample code from PosixExamples repo

--- PR03/src/depositer.h
+++ PR03/src/depositer.h
@@ -5,4 +5,6 @@
 #ifndef _ASN1_DEPOSITER_H_
 #define _ASN1_DEPOSITER_H_
 
+void depositor();
+
 #endif //_ASN1_DEPOSITER_H_
--- PR03/src/main.h
+++ PR03/src/main.h
@@ -5,4 +5,11 @@
 #ifndef _ASN1_MAIN_H_
 #define _ASN1_MAIN_H_
 
+#define DEPOSIT 0
+#define WITHDRAWAL 1
+
+int main();
+int randomType();
+int randomAmount();
+
 #endif //_ASN1_MAIN_H_
