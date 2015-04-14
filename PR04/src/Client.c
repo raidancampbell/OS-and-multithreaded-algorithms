2670b15041935182a4125b6f4a6d1d4a5e92674b
R. Aidan Campbell <aidan.campbell@gmail.com>
2015-04-14 13:12:26 -0400

    implemented list_all_messages

--- PR04/src/Client.c
+++ PR04/src/Client.c
@@ -27,6 +27,7 @@
 user* getUser();
 int callStart(user*, CLIENT*, int*);
 int callInsertMessage(user, char*, CLIENT*, int*);
+char* callListAllMessages(user*, CLIENT* );
 
 int main(int argc, char *argv[]) {
     CLIENT         *client;
@@ -60,7 +61,9 @@ int main(int argc, char *argv[]) {
     sleep(5u);
 
     //LIST_ALL_MESSAGES
-
+    char* string = callListAllMessages(myUser, client);
+    printf("\nclient: received messages.  Printing...");
+    printf(string);
 
     return (int) return_value;
 }
@@ -76,17 +79,26 @@ user* getUser(){
 
 
 int callStart(user* myUser, CLIENT* client, int* return_value){
-    printf("client : initializing START, with username: %s%d.\n", myUser->hostname, myUser->uuid);
+    printf("\nclient : initializing START, with username: %s%d.", myUser->hostname, myUser->uuid);
     return_value = start_1((void *) &myUser, client);
-    if (*return_value)  printf("client : START successful.\n");
-    else  printf("client : START failed.\n");
+    if (*return_value)  printf("\nclient : START successful.\n");
+    else  printf("\nclient : START failed.\n");
     return (int) return_value;
 }
 
 int callInsertMessage(user destinationUser, char* message, CLIENT* client, int* return_value){
-    printf("client : Inserting Message to username: %s%d.\n", destinationUser.hostname, destinationUser.uuid);
+    printf("\nclient : Inserting Message to username: %s%d.\n", destinationUser.hostname, destinationUser.uuid);
     return_value = insert_message_1(&((insert_message_params){.given_user=destinationUser, .message=message}), return_value);
-    if (*return_value)  printf("client : insert_message successful.\n");
-    else  printf("client : insert_message failed.\n");
+    if (*return_value)  printf("\nclient : insert_message successful.\n");
+    else  printf("\nclient : insert_message failed.\n");
     return (int) return_value;
+}
+
+char* callListAllMessages(user* myUser, CLIENT* client){
+    message_block * return_value = list_all_messages_1(myUser, client);
+    if(return_value == NULL) {
+        printf("\nclient : list_all_messages failed.");
+        return NULL;
+    }
+    return return_value->data;
 }
